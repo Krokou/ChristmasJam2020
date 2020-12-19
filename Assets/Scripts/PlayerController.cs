0e12255bb2d7ce2da705fcf95be7eed5584cf906
Krokou <asgeir@santi.no>
2020-12-19 22:19:16 +0100

    Ghost script and movement update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,39 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
-    
+
+    private int frame = 0;
+    private List<List<KeyCode>> recordedMovement = new List<List<KeyCode>>();
+
+    public List<List<KeyCode>> GetRecordedMovement()
+    {
+        return recordedMovement;
+    }
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    private void RecordMovement(KeyCode key)
     {
-        
+        recordedMovement[frame].Add(key);
     }
 
     private void FixedUpdate()
     {
+        recordedMovement.Add(new List<KeyCode>());
         if (Input.GetKey(KeyCode.W))
         {
             rb.velocity = new Vector3(rb.velocity.x, 0, 10);
+            RecordMovement(KeyCode.W);
         }
         else if (Input.GetKey(KeyCode.S))
         {
             rb.velocity = new Vector3(rb.velocity.x, 0, -10);
+            RecordMovement(KeyCode.S);
         }
         else rb.velocity = new Vector3(rb.velocity.x, 0, 0);
         if (Input.GetKey(KeyCode.A))
         {
             rb.velocity = new Vector3(-10, 0, rb.velocity.z);
+            RecordMovement(KeyCode.A);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector3(10, 0, rb.velocity.z);
+            RecordMovement(KeyCode.D);
         }
         else rb.velocity = new Vector3(0, 0, rb.velocity.z);
+
+        frame++;
     }
 }
