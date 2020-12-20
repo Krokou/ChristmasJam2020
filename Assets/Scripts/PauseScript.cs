f14f99132a6005416bf5b42d31b6a8aa9626efb9
Krokou <asgeir@santi.no>
2020-12-20 01:56:31 +0100

    Cleaned assets and file names

--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pause : MonoBehaviour
+public class PauseScript : MonoBehaviour
 
 {
     public static bool gameIsPaused;
